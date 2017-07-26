6e933f4d43ea3d4e8cdce3158af620c63e91100c
yeliex <yeliex@yeliex.com>
2017-07-26 11:07:46 +0800

    bug fix

--- src/libs/request.js
+++ src/libs/request.js
@@ -2,10 +2,10 @@ const global = require('./global');
 
 module.exports = (config) => ({ query = {}, params = {}, body = {}, headers = {}, fetch = config.fetch || global.fetch, mock } = {}) => {
   return fetch(config.path, {
-    mock,
-    query: Object.assign(config.query, query),
-    params: Object.assign(config.params, params),
-    body: Object.assign(config.body, body),
-    headers: Object.assign(config.headers, headers)
+    mock: mock === false ? mock : (mock || config.mock),
+    query: Object.assign({}, config.query, query),
+    params: Object.assign({}, config.params, params),
+    body: Object.assign({}, config.body, body),
+    headers: Object.assign({}, config.headers, headers)
   });
 };
